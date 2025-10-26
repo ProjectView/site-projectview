75641535ddb730f13a565d0f844d630504886ace
ProjectView <bernard@projectview.fr>
2025-10-26 18:27:24 +0100

    ✨ Create component: TestFinal2025

---
+++ src/components/TestFinal2025.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ArticleRenderer from './ArticleRenderer';
+
+const TestFinal2025 = () => {
+  return <ArticleRenderer articleId="test-final-2025" />;
+};
+
+export default TestFinal2025;
